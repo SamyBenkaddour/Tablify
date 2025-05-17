6c56b476b96665e0c8220fd10980ec6b92dc8bad
samy benkaddour <s.benkaddour10@gmail.com>
2025-05-17 21:53:36 +0100

    js to react

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+
+import Banner from './Tables/Banner'
+import Tables from './Tables/Tables'
+import Modal from './Tables/Modal'
+
+function App() {
+	
+
+	return (
+		<div>
+			<Banner/>   
+			<Tables/>
+			<Modal/>
+				
+			
+		
+		</div>
+	)
+}
+
+export default App
